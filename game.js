40d821b126243f05ba9a2fd28602ba593d6853cf
guneysrhat <guneysrhat@gmail.com>
2022-09-13 21:56:37 +0300

    again-btn

--- game.js
+++ game.js
@@ -4,40 +4,76 @@
 //*======================================
 
 //? 1-100 arasinda rasgele bir sayi tut.
-
 const randomNumber = Math.round(Math.random() * 100);
 console.log(randomNumber);
+
+//? Variables
+let score = 10;
+let topScore = 0;
 
 //* CheckBtn basildiginda kontrolleri yap
 document.querySelector(".check-btn").addEventListener("click", () => {
   const guessInput = Number(document.querySelector(".guess-input").value);
   const msg = document.querySelector(".msg");
-  //? eger input girilmediyse Kullaniciya uyari ver.
+  const body = document.querySelector("body");
 
+  //? eger input girilmediyse Kullaniciya uyari ver.
   if (!guessInput) {
     msg.innerText = "Please enter a number";
     //! eger rasgele == input.value
   } else if (randomNumber === guessInput) {
-    msg.innerText = "Congrats You Win";
-    document.querySelector("body").style.background = "green";
-    
+    msg.innerHTML = `Congrats You Win <i class="fa-solid fa-face-grin-hearts fa-2x"></i> `;
+    body.className = "bg-success";
+    document.querySelector(".check-btn").disabled = true;
+    if (score > topScore) {
+      topScore = score;
+      document.querySelector(".top-score").textContent = topScore;
+    }
+    document.querySelector(".secret-number").textContent = randomNumber;
+
+    //! eger rasgele!= input.value
+  } else {
+    score--;
+    if (score > 0) {
+      guessInput > randomNumber
+        ? (msg.innerHTML = `<i class="fa-solid fa-arrow-trend-down fa-2x"></i> DECREASE `)
+        : (msg.innerHTML = `<i class="fa-solid fa-arrow-trend-up fa-2x"></i> INCREASE `);
+    } else {
+      msg.innerHTML = `You Lost <i class="fa-regular fa-face-sad-tear fa-2x"></i>`;
+      document.querySelector(".secret-number").textContent = randomNumber;
+      body.className = "bg-danger";
+      document.querySelector(".check-btn").disabled = true;
+    }
+
+    document.querySelector(".score").textContent = score;
   }
 });
-//? tebrikler bildiiniz
-//? background = green
+
+//* again basildiginda oyunu baslangic dgerlerin kur
+document.querySelector(".again-btn").addEventListener("click", () => {
+  score = 10;
+  document.querySelector(".score").textContent = score;
+  const randomNumber = Math.round(Math.random() * 100);
+  document.querySelector(".secret-number").textContent = "?";
+  console.log(randomNumber);
+  document.querySelector(".check-btn").disabled = false;
+  document.querySelector("body").classList.remove("bg-success", "bg-danger");
+  document.querySelector(".guess-input").value = "";
+  document.querySelector(".msg").innerText = `Starting..`;
+});
 
 //? eger score > topScore
-//?    topScore = score
-//? secret_number = gorunur
+//?     topScore = score
+//? secret_number = gorunur.
 
-//! degilse
-//? eger score > 0
-//!   score = score - 1
-//?     eger rastgele < input.value
-//?         Azalt
-//?     degilse
-//?         Arttir
-//! degilse
-//? uzgunuz kaybettiniz
+//! değilse
+//! eger score > 0
+//!   score = score -1
+//?   eğer rasgele < input.value
+//?     AZALT
+//?   degilse
+//?     ARTTIR
+//! degise
+//? Uzgunuz kaybetiniz.
 
 //* againBtn basildiginda kontrolleri yap
