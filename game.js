d32f40c194810d99c8b6f7266e94be9e299a129f
guneysrhat <guneysrhat@gmail.com>
2022-09-13 20:43:51 +0300

    enter_number

--- game.js
+++ game.js
@@ -3,16 +3,26 @@
 //*  Game Logic
 //*======================================
 
-//? 1-100 Arasinda rastgele bir sayi tut.
+//? 1-100 arasinda rasgele bir sayi tut.
 
-
+const randomNumber = Math.round(Math.random() * 100);
+console.log(randomNumber);
 
 //* CheckBtn basildiginda kontrolleri yap
+document.querySelector(".check-btn").addEventListener("click", () => {
+  const guessInput = Number(document.querySelector(".guess-input").value);
+  const msg = document.querySelector(".msg");
+  //? eger input girilmediyse Kullaniciya uyari ver.
 
-//? eger input girilmediyse
-//? kullaniciya uyari ver
-
-//! eger rastgele == input.value
+  if (!guessInput) {
+    msg.innerText = "Please enter a number";
+    //! eger rasgele == input.value
+  } else if (randomNumber === guessInput) {
+    msg.innerText = "Congrats You Win";
+    document.querySelector("body").style.background = "green";
+    
+  }
+});
 //? tebrikler bildiiniz
 //? background = green
 
