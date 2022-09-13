120561c2b54ed54abaf20dd5cf97d1c01b4d649e
guneysrhat <guneysrhat@gmail.com>
2022-09-13 20:27:14 +0300

    pseudocode

--- game.js
+++ game.js
@@ -3,11 +3,31 @@
 //*  Game Logic
 //*======================================
 
-
 //? 1-100 Arasinda rastgele bir sayi tut.
 
 
 
+//* CheckBtn basildiginda kontrolleri yap
 
+//? eger input girilmediyse
+//? kullaniciya uyari ver
 
-//? 
+//! eger rastgele == input.value
+//? tebrikler bildiiniz
+//? background = green
+
+//? eger score > topScore
+//?    topScore = score
+//? secret_number = gorunur
+
+//! degilse
+//? eger score > 0
+//!   score = score - 1
+//?     eger rastgele < input.value
+//?         Azalt
+//?     degilse
+//?         Arttir
+//! degilse
+//? uzgunuz kaybettiniz
+
+//* againBtn basildiginda kontrolleri yap
